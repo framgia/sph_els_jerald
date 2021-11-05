2a6d4159cd80a6d5fb84d2b1bc6d0eaf676772f1
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-05 12:49:34 +0800

    Create API Get Quiz

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Quiz;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        Quiz::factory(5)->create();
     }
 }
