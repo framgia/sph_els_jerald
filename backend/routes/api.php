18f79caf020e9332e16ac970123dd05ab30cfa0f
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-05 12:49:34 +0800

    Create API Get Quiz

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Quiz\QuizController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/**
+ * Quizzes
+ */
+Route::resource('quizzes', QuizController::class)->only(['index', 'show']);
