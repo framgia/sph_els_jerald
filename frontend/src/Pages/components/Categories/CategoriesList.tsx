fdba8d5ca9704ba63e06de2d63a05c9062db39c6
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-08 13:09:05 +0800

    Integrate Fetch API in Categories

--- frontend/src/Pages/components/Categories/CategoriesList.tsx
+++ frontend/src/Pages/components/Categories/CategoriesList.tsx
@@ -1,66 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import { useAppSelector, useAppDispatch } from "../../../app/hooks";
+import {
+  selectQuizzes,
+  fetchQuizAsync,
+} from "../../../features/Categories/categoriesSlice";
+import { Quiz } from "../../../Types/Quiz";
+
 const CategoriesList = () => {
+  const data = useAppSelector(selectQuizzes);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchQuizAsync());
+  }, [dispatch]);
+
   return (
     <Fragment>
-      <div className="column">
-        <div className="ui segment raised very padded">
-          <h2 className="ui header">Basic 500</h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <Link to="/" className="ui primary button">
-            Start Lesson
-          </Link>
+      {data.status === "loading" && (
+        <div className="ui active dimmer">
+          <div className="ui loader"></div>
         </div>
-      </div>
-      <div className="column">
-        <div className="ui segment raised very padded">
-          <h2 className="ui header">Basic 500</h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <Link to="/" className="ui primary button">
-            Start Lesson
-          </Link>
-        </div>
-      </div>
-      <div className="column">
-        <div className="ui segment raised very padded">
-          <h2 className="ui header">Basic 500</h2>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <Link to="/" className="ui primary button">
-            Start Lesson
-          </Link>
-        </div>
-      </div>
+      )}
+
+      {data.quizzes &&
+        data.status === "idle" &&
+        data.quizzes.map((item: Quiz) => (
+          <div className="column" key={item.id}>
+            <div className="ui segment raised very padded">
+              <h2 className="ui header">{item.title}</h2>
+              <p>{item.description}</p>
+              <Link to="/" className="ui primary button">
+                Start Lesson
+              </Link>
+            </div>
+          </div>
+        ))}
+
+      {data.status === "idle" && !data.quizzes && (
+        <h2 className="ui message">No categories found</h2>
+      )}
     </Fragment>
   );
 };
---
+++ frontend/src/features/Categories/categoriesSlice.ts
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import { fetchQuiz } from "./categoriesAPI";
+import { Quiz } from "../../Types/Quiz";
+
+export interface QuizState {
+  quizzes: Quiz[];
+  status: "idle" | "loading" | "failed";
+}
+
+const initialState: QuizState = {
+  quizzes: [],
+  status: "idle",
+};
+
+export const fetchQuizAsync = createAsyncThunk(
+  "quizzes/fetchQuiz",
+  async () => {
+    const response = await fetchQuiz();
+    return response;
+  }
+);
+
+export const quizSlice = createSlice({
+  name: "quiz",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchQuizAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchQuizAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.quizzes = action.payload;
+      });
+  },
+});
+
+export const selectQuizzes = (state: RootState) => state.quiz;
+
+export default quizSlice.reducer;
