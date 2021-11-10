47e88252ec55df243ea8c22f63aba3776fe727ff
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-10 12:02:06 +0800

    Update Question Answer Page

--- frontend/src/Pages/StartLesson.tsx
+++ frontend/src/Pages/StartLesson.tsx
@@ -1,14 +1,33 @@
 import { Fragment } from "react";
-import StartLesson from "./components/StartLesson/StartLessonList";
+
+import classes from "./StartLesson.module.css";
 
 const Lesson = () => {
   return (
     <Fragment>
-      <form>
-        <div className="ui three column stackable grid">
-          <StartLesson />
+      <div className="ui container segment">
+        <div className="ui grid">
+          <div className="eight wide column centered">
+            <h2>Basic 500</h2>
+            <h1>Home</h1>
+          </div>
+          <div className="eight wide column centered">
+            <h2>1 out of 4</h2>
+            <div className={classes.choices}>
+              <button className="ui button primary">Balay</button>
+            </div>
+            <div className={classes.choices}>
+              <button className="ui button primary">Lingkuranan</button>
+            </div>
+            <div className={classes.choices}>
+              <button className="ui button primary">Lamesa</button>
+            </div>
+            <div className={classes.choices}>
+              <button className="ui button primary">Tindahan</button>
+            </div>
+          </div>
         </div>
-      </form>
+      </div>
     </Fragment>
   );
 };
---
+++ frontend/src/Types/Answer.ts
@@ -0,0 +1,9 @@
+export type Answer = {
+  isCorrect: number;
+  choiceId: number;
+};
+
+export const AnswerInitial = {
+  isCorrect: 0,
+  choiceId: 0,
+};
