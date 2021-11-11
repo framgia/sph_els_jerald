c67781a683dd61ee9551702a6eab8155112cf3b2
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-11 19:22:16 +0800

    [SELS TASK] Update Integrate Fetch API in Questions & Choices

--- frontend/src/features/StartLesson/lessonAPI.ts
+++ frontend/src/features/StartLesson/lessonAPI.ts
@@ -3,6 +3,7 @@ const axios = require("axios").default;
 export const fetchQuizDetail = (quizId: number) => {
   return axios.get(`http://127.0.0.1:8000/api/quizzes/${quizId}`);
 };
+
 export const fetchQuestions = (quizId: number) => {
-  return axios.get(`http://127.0.0.1:8000/api/quizzes/${quizId}`);
+  return axios.get(`http://127.0.0.1:8000/api/quizzes/${quizId}/questions`);
 };
