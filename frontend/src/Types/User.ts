619cb73a023f835fa19572b0dcf13ed498c2719b
jeraldechavia <92574920+jeraldechavia@users.noreply.github.com>
2021-11-15 18:20:40 +0800

    [SELS TASK] Integrate API User Sign Up (#19)
    * [SELS TASK] Create API to Register User
    
    * [SELS TASK] Integrate API User Sign Up

---
+++ frontend/src/Types/User.ts
@@ -0,0 +1,15 @@
+export type User = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+export const UserInitial = {
+  firstName: "",
+  middleName: "",
+  lastName: "",
+  email: "",
+  password: "",
+};
