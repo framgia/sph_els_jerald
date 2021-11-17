0c7a3532b40742015372ac80f98c75e40cd63fa6
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-17 10:56:43 +0800

    [SELS TASK] Axios Error TypeScript

--- frontend/src/features/SignIn/signInAPI.ts
+++ frontend/src/features/SignIn/signInAPI.ts
@@ -1,7 +1,10 @@
+import { AxiosError } from "axios";
 import axios from "../../axios/api";
 
 export const signInUser = (data: { email: string; password: string }) => {
-  return axios.post("/api/users/signin", data).catch(function (error: any) {
-    return error.response;
-  });
+  return axios
+    .post("/api/users/signin", data)
+    .catch(function (error: AxiosError) {
+      return error.response;
+    });
 };
