df96be90e1eeaa01bdced41543ab7d1797d7bae7
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-18 10:18:01 +0800

    [SELS TASK] Create API Get Admin Question & Choices

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -37,6 +37,7 @@ Route::post('admin/quizzes', [AdminQuizController::class, 'storeAdminQuiz'])->na
 Route::delete('admin/quizzes/{quiz}', [AdminQuizController::class, 'deleteAdminQuiz'])->name('admin.quizzes.destroy');
 Route::patch('admin/quizzes/{quiz}', [AdminQuizController::class, 'updateAdminQuiz'])->name('admin.quizzes.update');
 Route::get('admin/quizzes/{quiz}/questions', [AdminQuizController::class, 'showAdminQuizQuestions'])->name('admin.quizzes.questions.index');
+Route::get('admin/quizzes/questions/{question}', [AdminQuizController::class, 'showAdminQuizQuestion'])->name('admin.quizzes.questions.show');
 Route::post('admin/quizzes/{quiz}/questions', [AdminQuizController::class, 'storeAdminQuizQuestion'])->name('admin.quizzes.questions.store');
 Route::delete('admin/quizzes/questions/{question}', [AdminQuizController::class, 'deleteAdminQuizQuestion'])->name('admin.quizzes.questions.destroy');
 
