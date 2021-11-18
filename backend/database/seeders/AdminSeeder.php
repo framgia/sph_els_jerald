304aee014b69d4d3e858541464e84af618771c41
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-11-18 18:18:39 +0800

    [SELS TASK] Create API Admin Sign In and Sign Out

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'firstName' => 'Jerald Joshua',
+            'middleName' => 'Corpuz',
+            'lastName' => 'Echavia',
+            'email' => 'admin@elearning.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'isAdmin' => true,
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
