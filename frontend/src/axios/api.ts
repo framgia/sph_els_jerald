35e4e5a04dd89e3e412df12660581bc2f2b5fe1a
jeraldechavia <jeraldjoshua.echavia@sun-asterisk.com>
2021-12-03 11:16:25 +0800

    [SELS TASK] Adding .env file in frontend

--- frontend/src/axios/api.ts
+++ frontend/src/axios/api.ts
@@ -3,7 +3,7 @@ import CookieService from "../Services/CookieService";
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
 instance.interceptors.request.use(
